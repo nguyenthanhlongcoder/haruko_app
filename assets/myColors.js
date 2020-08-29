95e6d01817d12ac46fa1dd1e21e101db309b50e0
= <=>
2020-08-29 11:00:31 +0700

    add folder components, screens and file colors

---
+++ assets/myColors.js
@@ -0,0 +1,10 @@
+export const myColors = {
+    darkPrimaryColor: '#0288D1',
+    defaultPrimaryColor: '#03A9F4',
+    lightPrimaryColor: '#B3E5FC',
+    textPrimaryColor: '#FFFFFF',
+    accentColor: '#448AFF',
+    primaryTextColor: '#212121',
+    secondaryTextColor: '#757575',
+    dividerColor: '#BDBDBD'
+}
