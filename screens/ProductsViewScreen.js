6e8d075b7620c5911adbb5255648349364345715
phuocNguyen2000 <phuocnguyen822@gmail.com>
2020-09-04 00:02:20 +0700

    search

--- screens/ProductsViewScreen.js
+++ screens/ProductsViewScreen.js
@@ -13,36 +13,40 @@ import CategoryItem from "../components/CategoryItem";
 import { myColors } from "../assets/myColors";
 import IconAntDesign from "react-native-vector-icons/AntDesign";
 import SearchBar from "../components/SearchBar";
-import MyStatusBar from '../components/MyStatusBar';
-import {firebaseApp} from '../components/FirebaseConfig';
-import Icon from 'react-native-vector-icons/Foundation';
+import MyStatusBar from "../components/MyStatusBar";
+import { firebaseApp } from "../components/FirebaseConfig";
+import Icon from "react-native-vector-icons/Foundation";
 import { element } from "prop-types";
 
 export default class ProductsViewScreen extends React.Component {
-
   constructor() {
     super();
     this.state = {
       iconName: "right",
       cateListHeight: 0,
-      cateGory:[]
+      cateGory: [],
     };
   }
   componentDidMount() {
     this.defaultLoadData();
   }
   defaultLoadData = () => {
-    let catelist = [];
+    let catelist = [
+      {
+        Title: "ALL",
+        isChecked: true,
+      },
+    ];
     let Productlist = [];
     firebaseApp
       .database()
       .ref("/Shop/")
       .on("value", (data) => {
-        data.child("Category").forEach((element) => { 
-          var cate={
-            Title:'',
-            isChecked:false
-          }
+        data.child("Category").forEach((element) => {
+          var cate = {
+            Title: "",
+            isChecked: false,
+          };
           cate.Title = element.val().Title;
           catelist.push(cate);
         });
@@ -60,32 +64,66 @@ export default class ProductsViewScreen extends React.Component {
           product.content = element.val().Title;
           product.price = element.val().Price;
           product.description = element.val().Description;
+          product.avatar = element.val().Avatar;
 
           Productlist.push(product);
         });
-      
-        this.setState({ data:catelist, product: Productlist });
+
+        this.setState({ data: catelist, product: Productlist });
       });
   };
-  onCateItemPress = (item,ind) => {
-    let Productlist = [];
-    let cateList=[]
-    
-   for( var i=0;i<this.state.data.length;i++)
-   {
-     if(i===ind)
-     {
-       this.state.data[ind].isChecked=true;
-     }
-     else{
-      this.state.data[i].isChecked=false;
-     }
-   }
+  onCateItemPress = (item, ind) => {
+    var Productlist = [];
+  
+    for (var i = 0; i < this.state.data.length; i++) {
+      if (i === ind) {
+        this.state.data[ind].isChecked = true;
+      } else {
+        this.state.data[i].isChecked = false;
+      }
+    }
 
-this.setState({
-  data:this.state.data,
-})
-console.log(this.state.data[ind].isChecked)
+    this.setState({
+      data: this.state.data,
+    });
+
+    console.log(this.state.data[ind].isChecked);
+    firebaseApp
+      .database()
+      .ref("/Shop/")
+      .on("value", (data) => {
+        data.child("Product").forEach((element) => {
+          var product = {
+            content: "",
+            image: "https://cf.shopee.vn/file/ead47f6e94606a532bdb90cfeff5da8a",
+            content: "",
+            price: "",
+            sold: 8,
+            category: "",
+            description: "",
+            avatar:''
+          };
+          if (element.val().Category === item.Title) {
+            product.content = element.val().Title;
+            product.price = element.val().Price;
+            product.description = element.val().Description;
+            product.avatar = element.val().Avatar;
+            Productlist.push(product);
+          
+          }
+          if (item.Title === "ALL") {
+            product.content = element.val().Title;
+            product.price = element.val().Price;
+            product.description = element.val().Description;
+            product.avatar = element.val().Avatar;
+            Productlist.push(product);
+          }
+        });
+        this.setState({ product: Productlist });
+      });
+  };
+search=(inputText)=>{
+  var Productlist = [];
   firebaseApp
       .database()
       .ref("/Shop/")
@@ -99,28 +137,26 @@ console.log(this.state.data[ind].isChecked)
             sold: 8,
             category: "",
             description: "",
+            avatar:''
           };
-          if (element.val().Category === item.Title) {
+          if (element.val().Title.toUpperCase().search(inputText.toUpperCase())!=-1) {
             product.content = element.val().Title;
             product.price = element.val().Price;
             product.description = element.val().Description;
+            product.avatar = element.val().Avatar;
             Productlist.push(product);
+          
           }
         });
         this.setState({ product: Productlist });
       });
-     
-  };
-  _scrollEnd = (evt) => {
-    this.refs.flatList1.scrollToEnd();
-  }
+
+}
   render() {
     return (
       <View style={styles.container}>
         <MyStatusBar />
-        <SearchBar
-          onPress={() => this.props.navigation.navigate("Third")}
-        />
+        <SearchBar onSearch={this.search} onPress={() => this.props.navigation.navigate("Third")} />
         <View
           onTouchEnd={() => {
             if (
@@ -129,7 +165,7 @@ console.log(this.state.data[ind].isChecked)
             ) {
               this.setState({
                 iconName: "down",
-                cateListHeight: Dimensions.get("window").height / 10,
+                cateListHeight: Dimensions.get("window").height / 8,
               });
             } else {
               this.setState({ iconName: "right", cateListHeight: 0 });
@@ -139,12 +175,17 @@ console.log(this.state.data[ind].isChecked)
             flexDirection: "row",
             alignItems: "center",
             padding: 5,
-            backgroundColor: '#fff',
-            justifyContent: 'center'
+            backgroundColor: "#fff",
+            justifyContent: "center",
           }}
         >
-          <Icon name='list-bullet' size={25} color={myColors.defaultPrimaryColor} style={{marginHorizontal:5}}/>
-          <Text style={{fontSize: 25 , opacity: .7}}>Category</Text>
+          <Icon
+            name="list-bullet"
+            size={25}
+            color={myColors.defaultPrimaryColor}
+            style={{ marginHorizontal: 5 }}
+          />
+          <Text style={{ fontSize: 25, opacity: 0.7 }}>Category</Text>
           <IconAntDesign
             name={this.state.iconName}
             style={{ fontSize: 20, flex: 1, textAlign: "right" }}
@@ -152,16 +193,17 @@ console.log(this.state.data[ind].isChecked)
         </View>
         <View style={{ height: this.state.cateListHeight, width: "100%" }}>
           <FlatList
-         
             horizontal={true}
             data={this.state.data}
             ref="cateList"
-        onScrollAnimationEnd={()=>{this.refs.cateList.scrollTop}}
-            renderItem={({ item,index }) => {
+            onScrollAnimationEnd={() => {
+              this.refs.cateList.scrollTop;
+            }}
+            renderItem={({ item, index }) => {
               return (
                 <CategoryItem
                   onPress={() => {
-                    this.onCateItemPress(item,index);
+                    this.onCateItemPress(item, index);
                   }}
                   item={item}
                 />
@@ -182,7 +224,9 @@ console.log(this.state.data[ind].isChecked)
             return (
               <Product
                 item={item}
-                onPress={() => this.props.navigation.navigate("ProductDetailScreen", item)}
+                onPress={() =>
+                  this.props.navigation.navigate("ProductDetailScreen", item)
+                }
               />
             );
           }}
@@ -194,8 +238,7 @@ console.log(this.state.data[ind].isChecked)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-  
+    flexDirection: "column",
   },
   scrollViewStyle: {
     justifyContent: "flex-end",
